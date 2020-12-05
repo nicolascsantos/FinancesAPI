179ef5e0d58c523d2f5fb45bb0f2fbd6df9840a3
Nicolas Carvalho <nicolas.carvalho@hotmail.com.br>
2020-12-05 16:48:39 -0300

    Identity migration added.

--- FinancesApi/Data/ContextoDB.cs
+++ FinancesApi/Data/ContextoDB.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using FinancesApi.Models;
 
 namespace FinancesApi.Data
 {
-    public class ContextoDB : DbContext
+    public class ContextoDB : IdentityDbContext
     {
         public ContextoDB(DbContextOptions options): base(options)
         {
--- FinancesApi/Migrations/20201205194547_Identity.Designer.cs
+++ FinancesApi/Migrations/20201205194547_Identity.Designer.cs
@@ -4,14 +4,16 @@ using FinancesApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FinancesApi.Migrations
 {
     [DbContext(typeof(ContextoDB))]
-    partial class ContextoDBModelSnapshot : ModelSnapshot
+    [Migration("20201205194547_Identity")]
+    partial class Identity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
